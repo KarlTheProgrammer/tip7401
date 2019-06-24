e85da930f1baf739c3f3002d5e72d0f023b41db7
Curtis Ellis <curtis@nextcash.tech>
2019-06-23 22:49:12 -0600

    Start implementing manual key input.

--- src/encryption.js
+++ src/encryption.js
@@ -1,141 +1,81 @@
 
 
-async function run() {
-    ////////////////////////////////////////// Setup Keys //////////////////////////////////////////
-    var sender = await window.crypto.subtle.generateKey(
-        {
-            name: "ECDSA",
-            namedCurve: "P-256", //can be "P-256", "P-384", or "P-521"
-        },
-        true, //whether the key is extractable (i.e. can be used in exportKey)
-        ["sign", "verify"]
-    );
-    var receiver1 = await window.crypto.subtle.generateKey(
-        {
-            name: "ECDSA",
-            namedCurve: "P-256", //can be "P-256", "P-384", or "P-521"
-        },
-        true, //whether the key is extractable (i.e. can be used in exportKey)
-        ["sign", "verify"]
-    );
-    var receiver2 = await window.crypto.subtle.generateKey(
-        {
-            name: "ECDSA",
-            namedCurve: "P-256", //can be "P-256", "P-384", or "P-521"
-        },
-        true, //whether the key is extractable (i.e. can be used in exportKey)
-        ["sign", "verify"]
-    );
+async function generateAll() {
+    generateSender();
+    generateReceiver1();
+    generateReceiver2();
+    generateMaster();
+}
 
-    document.getElementById('sender_priv').innerHTML = await privateText(sender.privateKey);
-    document.getElementById('sender_pub_x').innerHTML = await publicXText(sender.publicKey);
-    document.getElementById('sender_pub_y').innerHTML = await publicYText(sender.publicKey);
+async function generateSender() {
+    var sender = bsv.HDPrivateKey.fromRandom();
+    document.getElementById('sender_priv').value = sender.toString();
+}
 
-    document.getElementById('receiver1_priv').innerHTML = await privateText(receiver1.privateKey);
-    document.getElementById('receiver1_pub_x').innerHTML = await publicXText(receiver1.publicKey);
-    document.getElementById('receiver1_pub_y').innerHTML = await publicYText(receiver1.publicKey);
+async function generateReceiver1() {
+    var receiver1 = bsv.HDPrivateKey.fromRandom();
+    document.getElementById('receiver1_priv').value = receiver1.toString();
 
-    document.getElementById('receiver2_priv').innerHTML = await privateText(receiver2.privateKey);
-    document.getElementById('receiver2_pub_x').innerHTML = await publicXText(receiver2.publicKey);
-    document.getElementById('receiver2_pub_y').innerHTML = await publicYText(receiver2.publicKey);
+    var receiver1_pub = bsv.HDPublicKey.fromHDPrivateKey(receiver1);
+    document.getElementById('receiver1_pub').value = receiver1_pub.toString();
+}
 
-    //////////////////////////////////////// Derive Secrets ////////////////////////////////////////
-    var sender_secret1 = await secret(sender.privateKey, receiver1.publicKey);
-    var sender_key1 = await window.crypto.subtle.digest(
-        {
-            name: "SHA-256",
-        },
-        new Uint8Array(sender_secret1)
-    );
-    document.getElementById('sender_secret1').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(sender_secret1)));
+async function generateReceiver2() {
+    var receiver2 = bsv.HDPrivateKey.fromRandom();
+    document.getElementById('receiver2_priv').value = receiver2.toString();
 
-    var sender_secret2 = await secret(sender.privateKey, receiver2.publicKey);
-    var sender_key2 = await window.crypto.subtle.digest(
-        {
-            name: "SHA-256",
-        },
-        new Uint8Array(sender_secret2)
-    );
-    document.getElementById('sender_secret2').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(sender_secret2)));
+    var receiver2_pub = bsv.HDPublicKey.fromHDPrivateKey(receiver2);
+    document.getElementById('receiver2_pub').value = receiver2_pub.toString();
+}
 
-    var receiver1_secret = await secret(receiver1.privateKey, sender.publicKey);
-    var receiver1_key = await window.crypto.subtle.digest(
-        {
-            name: "SHA-256",
-        },
-        new Uint8Array(receiver1_secret)
-    );
-    document.getElementById('receiver1_secret').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(receiver1_secret)));
-
-    var receiver2_secret = await secret(receiver2.privateKey, sender.publicKey);
-    var receiver2_key = await window.crypto.subtle.digest(
-        {
-            name: "SHA-256",
-        },
-        new Uint8Array(receiver2_secret)
-    );
-    document.getElementById('receiver2_secret').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(receiver2_secret)));
-
-    /////////////////////////////////////// Encrypt Master Key /////////////////////////////////////
+async function generateMaster() {
     var master_secret = window.crypto.getRandomValues(new Uint8Array(32));
-    var master_key = await window.crypto.subtle.digest(
+    document.getElementById('master_key').innerHTML = toHexString(master_secret);
+}
+
+async function generateEncryptMessage() {
+    var key_data = toByteArray(document.getElementById('master_key').innerHTML);
+    var key = await window.crypto.subtle.digest(
         {
             name: "SHA-256",
         },
-        new Uint8Array(master_secret)
+        new Uint8Array(key_data)
     );
-    document.getElementById('master_key').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(master_key)));
 
-    /////////////////////////////////////// Decrypt Master Key /////////////////////////////////////
-    var encrypted1 = await encrypt(sender_key1, master_key);
-    var iv1 = encrypted1[0];
-    var encrypted_key1 = encrypted1[1];
-    var decrypted_key1 = await decrypt(receiver1_key, iv1, encrypted_key1);
-    document.getElementById('master_key1').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(decrypted_key1)));
-
-    var encrypted2 = await encrypt(sender_key2, master_key);
-    var iv2 = encrypted2[0];
-    var encrypted_key2 = encrypted2[1];
-    var decrypted_key2 = await decrypt(receiver2_key, iv2, encrypted_key2);
-    document.getElementById('master_key2').innerHTML = window.btoa(String.fromCharCode(...new Uint8Array(decrypted_key2)));
-
-    ///////////////////////////////////////// Encrypt Message //////////////////////////////////////
-    var message = "Shh. Dont let them see this.";
-    document.getElementById('sender_message').innerHTML = message;
-
+    var message = document.getElementById('message').value;
     var message_data = new TextEncoder().encode(message);
-    var encrypted_message = await encrypt(master_key, message_data);
-    var message_iv = encrypted_message[0];
-    var encrypted_message = encrypted_message[1];
-
-    var decrypted_message1 = await decrypt(decrypted_key1, message_iv, encrypted_message);
-    document.getElementById('receiver1_message').innerHTML = new TextDecoder("utf-8").decode(decrypted_message1);
-
-    var decrypted_message2 = await decrypt(decrypted_key2, message_iv, encrypted_message);
-    document.getElementById('receiver2_message').innerHTML = new TextDecoder("utf-8").decode(decrypted_message2);
+    var encrypted_message = await encrypt(key, message_data);
+    document.getElementById('message_iv').innerHTML = toHexString(encrypted_message[0]);
+    document.getElementById('encrypted_message').innerHTML = toHexString(new Uint8Array(encrypted_message[1]));
 }
 
-async function privateText(key) {
-    var data = await window.crypto.subtle.exportKey("jwk", key);
-    return data.d;
+async function encryptReceiver1() {
+    var senderPriv = bsv.HDPrivateKey.fromString(document.getElementById('sender_priv').value).privateKey;
+    var receiver1Pub = bsv.HDPublicKey.fromString(document.getElementById('receiver1_pub').value).publicKey;
+    var key = await dhKey(senderPriv, receiver1Pub);
+    document.getElementById('sender_receiver1_ecdh').innerHTML = toHexString(key);
+
+    var master_key = toByteArray(document.getElementById('master_key').innerHTML);
+
+    var encrypted = await encrypt(key, master_key);
+    document.getElementById('receiver1_IV').innerHTML = toHexString(encrypted[0]);
+    document.getElementById('receiver1_encrypted_key').innerHTML = toHexString(encrypted[1]);
 }
 
-async function publicXText(key) {
-    var data = await window.crypto.subtle.exportKey("jwk", key);
-    return data.x;
-}
-
-async function publicYText(key) {
-    var data = await window.crypto.subtle.exportKey("jwk", key);
-    return data.y;
-}
-
-async function secret(privateKey, publicKey) {
+async function dhKey(privateKey, publicKey) {
 
     // Convert keys from ECDSA to ECDH
-    var privdata = await window.crypto.subtle.exportKey("pkcs8", privateKey);
+    // var privdata = await window.crypto.subtle.exportKey("raw", privateKey);
+    var privBuffer = privateKey.toBuffer();
+    console.log("privBuffer")
+    console.log(privBuffer)
+    var der = new Uint8Array(privBuffer.length + 1)
+    // der[0] = 0 // Set first byte zero
+    // der.set(privBuffer, 1)
+    // console.log("der")
+    // console.log(der)
     var privkey = await window.crypto.subtle.importKey(
-        "pkcs8", privdata,
+        "raw", privBuffer,
         {   //these are the algorithm options
             name: "ECDH",
             namedCurve: "P-256", //can be "P-256", "P-384", or "P-521"
@@ -146,9 +86,9 @@ async function secret(privateKey, publicKey) {
     // console.log("privkey");
     // console.log(privkey);
 
-    var pubdata = await window.crypto.subtle.exportKey("spki", publicKey);
+    // var pubdata = await window.crypto.subtle.exportKey("spki", publicKey);
     var pubkey = await window.crypto.subtle.importKey(
-        "spki", pubdata,
+        "spki", publicKey.toDER(),
         {   //these are the algorithm options
             name: "ECDH",
             namedCurve: "P-256", //can be "P-256", "P-384", or "P-521"
@@ -169,15 +109,23 @@ async function secret(privateKey, publicKey) {
         256 //the number of bits you want to derive
     );
 
-    // console.log("secret");
-    // console.log(secret);
-    return secret;
+    var key = await window.crypto.subtle.digest(
+        {
+            name: "SHA-256",
+        },
+        new Uint8Array(secret)
+    );
+
+    console.log("secret");
+    console.log(secret);
+    console.log("key");
+    console.log(key);
+    return key;
 }
 
 async function encrypt(keyValue, data) {
     var key  = await window.crypto.subtle.importKey(
-        "raw", //can be "jwk" or "raw"
-        keyValue,
+        "raw", keyValue,
         {   //this is the algorithm options
             name: "AES-CBC",
         },
@@ -221,4 +169,18 @@ async function decrypt(keyValue, iv, data) {
     );
 
     return decrypted;
+}
+
+function toHexString(byteArray) {
+  return Array.prototype.map.call(byteArray, function(byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('');
+}
+
+function toByteArray(hexString) {
+  var result = [];
+  for (var i = 0; i < hexString.length; i += 2) {
+    result.push(parseInt(hexString.substr(i, 2), 16));
+  }
+  return result;
 }
